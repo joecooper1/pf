e3ee3c24c0851accf0b41df71cd062cf521d51ac
Joe Cooper <joecooper2991@gmail.com>
2020-05-21 19:22:02 +0100

    add variables to text size

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -5,7 +5,7 @@ import { Title, Text, View, Contacts } from "../styles/Bio";
 export default function Bio(props) {
   return (
     <View>
-      <Title>Joe Cooper</Title>
+      <Title dimensions={props}>Joe Cooper</Title>
       <Text dimensions={props}>Hi. I'm Joe.<br></br> I'm a full-stack software developer.</Text>
       <Contacts dimensions={props}></Contacts>
     </View>
--- src/styles/Bio.js
+++ src/styles/Bio.js
@@ -1,13 +1,18 @@
 import styled from "styled-components";
 
 export const Title = styled.h1`
-  font-size: 1.5em;
+  font-size: ${props=> {
+    return props.dimensions.width > 1024 ? '2.5em' : '1.5em'
+  }};
   text-align: center;
   color: palevioletred;
+  margin: 0;
 `;
 
 export const Text = styled.p`
-  font-size: 1em;
+  font-size: ${props=> {
+    return props.dimensions.width > 1024 ? '1em' : '0.5em'
+  }};
   text-align: center;
   color: palevioletred;
 `;
