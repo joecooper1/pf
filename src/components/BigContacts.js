a26699473598a28bd5f31d7cd17a8a425e552651
Joe Cooper <joecooper2991@gmail.com>
2020-05-21 19:32:54 +0100

    opened up nav bar to things inside it

--- src/components/BigContacts.js
+++ src/components/BigContacts.js
@@ -3,5 +3,5 @@ import React from "react";
 import {BigContactStyle} from '../styles/Contacts';
 
 export default function BigContacts(props) {
-    return (<BigContactStyle dimensions={props} />)
+    return (<BigContactStyle dimensions={props}></BigContactStyle>)
 };
