f0ace86c4510c24ddc10677a79649b50e57f0db0
Joe Cooper <joecooper2991@gmail.com>
2020-05-21 19:38:30 +0100

    add text styled component

--- src/components/BigContacts.js
+++ src/components/BigContacts.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-import {BigContactStyle} from '../styles/Contacts';
+import {BigContactStyle, Text} from '../styles/Contacts';
 
 export default function BigContacts(props) {
-    return (<BigContactStyle dimensions={props}></BigContactStyle>)
+    return (<BigContactStyle dimensions={props}>
+        <Text>email: joecooper2991@gmail.com</Text>
+    </BigContactStyle>)
 };
--- src/styles/Contacts.js
+++ src/styles/Contacts.js
@@ -3,10 +3,15 @@ import styled from "styled-components";
 //Contacts bar. Dimensions is passed through. Variables depend on dimensions width.
 export const BigContactStyle = styled.nav`
   display: flex;
-  flex-direction: 'row';
-  background-color: white;
+  flex-direction: 'column';
   width: ${(props) => {
     return `${props.dimensions.width * 0.6}px`
   }};
   height: 100px;
+  justify-content: flex-end;
+`;
+
+export const Text = styled.p`
+  color: black;
+  font-size: 0.5em;
 `;
