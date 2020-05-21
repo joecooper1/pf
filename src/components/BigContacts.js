9c19d3a1da9b78620bea358c2362ac135f0fca5e
Joe Cooper <joecooper2991@gmail.com>
2020-05-21 19:30:08 +0100

    create new component for contacts nav bar for big screen

---
+++ src/components/BigContacts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import {BigContactStyle} from '../styles/Contacts';
+
+export default function BigContacts(props) {
+    return (<BigContactStyle dimensions={props} />)
+};
