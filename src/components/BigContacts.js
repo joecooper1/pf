6bc1b3737844a12c720a11aea11526631563c46c
Joe Cooper <joecooper2991@gmail.com>
2020-05-21 19:45:00 +0100

    add second link

--- src/components/BigContacts.js
+++ src/components/BigContacts.js
@@ -5,5 +5,6 @@ import {BigContactStyle, Text} from '../styles/Contacts';
 export default function BigContacts(props) {
     return (<BigContactStyle dimensions={props}>
         <Text>email: joecooper2991@gmail.com</Text>
+        <Text>github: www.github.com/joecooper1</Text>
     </BigContactStyle>)
 };
--- src/styles/Contacts.js
+++ src/styles/Contacts.js
@@ -3,12 +3,13 @@ import styled from "styled-components";
 //Contacts bar. Dimensions is passed through. Variables depend on dimensions width.
 export const BigContactStyle = styled.nav`
   display: flex;
-  flex-direction: 'column';
+  flex-direction: column;
   width: ${(props) => {
     return `${props.dimensions.width * 0.6}px`
   }};
   height: 100px;
   justify-content: flex-end;
+  align-items: flex-end;
 `;
 
 export const Text = styled.p`
