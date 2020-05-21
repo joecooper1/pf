02420ac1ee79c5a3713b3493f398d6468467659e
Joe Cooper <joecooper2991@gmail.com>
2020-05-21 19:15:05 +0100

    add line break

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -6,7 +6,7 @@ export default function Bio(props) {
   return (
     <View>
       <Title>Joe Cooper</Title>
-      <Text>Hi. I'm Joe. I'm a full-stack software developer.</Text>
+      <Text dimensions={props}>Hi. I'm Joe.<br></br> I'm a full-stack software developer.</Text>
       <Contacts dimensions={props}></Contacts>
     </View>
   );
