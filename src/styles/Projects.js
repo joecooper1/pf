32924ef828d6a93877e0bf7d9d02c9edc204c6bc
Joe Cooper <joecooper2991@gmail.com>
2020-05-22 13:25:29 +0100

    set flexbox properties for projects box

--- src/styles/Projects.js
+++ src/styles/Projects.js
@@ -1,9 +1,18 @@
 import styled from "styled-components";
 
 export const Box = styled.div`
-  width: ${props => `${props.dimensions.width* 0.95}px`};
+  width: ${(props) => `${props.dimensions.width * 0.95}px`};
   background-color: red;
   min-height: 150px;
   margin-bottom: 100px;
-
+  display: flex;
+  flex-direction: ${(props) => {
+    return props.dimensions.width > 1024 ? "row" : "column";
+  }};
+  justify-content: ${(props) => {
+    return props.dimensions.width > 1024 ? "space-around" : "flex-start";
+  }};
+  flex-wrap: wrap;
+  align-items: stretch;
+  align-content: space-around;
 `;
