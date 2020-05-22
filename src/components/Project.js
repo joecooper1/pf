7f86bf585010a6f891225d47972b52759eab3d16
Joe Cooper <joecooper2991@gmail.com>
2020-05-22 20:09:38 +0100

    change github link to say view code on small screen

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,13 +3,14 @@ import React from "react";
 import { ProjectStyle, Title, Link, Image, Info, Text } from "../styles/Projects";
 
 export default function Project(props) {
-  const { data } = props;
+  const { data, dimensions } = props;
+  const githubLink = dimensions.width > 1024 ? data.github : 'View code';
   return (
-    <ProjectStyle width={props.dimensions.width}>
+    <ProjectStyle width={dimensions.width}>
       <Image src={data.url} alt={`Screenshot of ${data.name}`} />
       <Info>
         <Title>{data.name}</Title>
-        <Link href={data.github}>{data.github}</Link>
+        <Link href={data.github}>{githubLink}</Link>
         <Text>{data.description}</Text>
       </Info>
     </ProjectStyle>
