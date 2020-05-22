515a87ef68bc5728c7daaa7094067ebc32e1a691
Joe Cooper <joecooper2991@gmail.com>
2020-05-22 12:33:06 +0100

    create project box component

---
+++ src/components/ProjectBox.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import {} from "../styles/Projects";
+
+export default function projectBox(props) {
+  return <Box dimensions={props}></Box>;
+}
---
+++ src/styles/Projects.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Box = styled.div`
+  width: ${props => `${props.dimensions.width}px`};
+  background-color: red;
+  height: 50;
+`;
