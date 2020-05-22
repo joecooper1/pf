8ceed9eef8953b5a3d645df2bfc603a57fbad9e3
Joe Cooper <joecooper2991@gmail.com>
2020-05-22 13:29:28 +0100

    pass through dimensions

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,6 +2,6 @@ import React from "react";
 
 import { Project } from "../styles/Projects";
 
-export default function Project() {
-    return <Project></Project>
+export default function Project(props) {
+    return <Project dimensions={props}></Project>
 };
