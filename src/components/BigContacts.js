e6173feb9899ee768793fbae0fd05a7bc9a2dc73
Joe Cooper <joecooper2991@gmail.com>
2020-05-22 11:53:31 +0100

    add more links to bio

--- src/components/BigContacts.js
+++ src/components/BigContacts.js
@@ -5,6 +5,7 @@ import {BigContactStyle, Text} from '../styles/Contacts';
 export default function BigContacts(props) {
     return (<BigContactStyle dimensions={props}>
         <Text>email: joecooper2991@gmail.com</Text>
-        <Text>github: www.github.com/joecooper1</Text>
+        <Text>github: <a href="https://www.github.com/joecooper1">www.github.com/joecooper1</a></Text>
+        <Text>linkedin: <a href="https://www.linkedin.com/in/joe-cooper-202b2689/">https://www.linkedin.com/in/joe-cooper-202b2689/</a></Text>
     </BigContactStyle>)
 };
