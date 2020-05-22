434bbb485e7fa7e8b6f622aa3c69c27c39a45776
Joe Cooper <joecooper2991@gmail.com>
2020-05-22 15:16:51 +0100

    add project data file

---
+++ src/data/projectData.js
@@ -0,0 +1,18 @@
+const projectData = [
+  {
+    name: "Only Reggae",
+    github: "https://github.com/joecooper1/ToteTechTestApp",
+    link: null,
+    description:
+      "A small music app I made in four days for a tech test.\nIt uses the Napster API to display the top Reggae tracks, charts and playlists, and gives users the ability to search the database for their favourite albums, artists and songs. Built using React Native.",
+    url: null      
+  },
+  {
+    name: "uFluent",
+    github: "https://github.com/uFluent/frontend",
+    link: null,
+    description:
+      "An educational app designed to help children learn new vocabulary in various languages.\nThe in-built camera app uses machine learning to reocgnise and translate household objects, and vocabulary quizzes help kids to memorise words.\nBuilt as a final project at Northcoders bootcamp with a team of four other people, in React Native and Python.",
+    url: null      
+  },
+];
