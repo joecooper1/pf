b105282aa5009361b78833fc8f3bbc04801861a0
Joe Cooper <joecooper2991@gmail.com>
2020-05-22 15:19:05 +0100

    add project urls

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,11 +1,11 @@
-const projectData = [
+export const projectData = [
   {
     name: "Only Reggae",
     github: "https://github.com/joecooper1/ToteTechTestApp",
     link: null,
     description:
       "A small music app I made in four days for a tech test.\nIt uses the Napster API to display the top Reggae tracks, charts and playlists, and gives users the ability to search the database for their favourite albums, artists and songs. Built using React Native.",
-    url: null      
+    url: '../../public/images/onlyReggae.jpg'    
   },
   {
     name: "uFluent",
@@ -13,6 +13,6 @@ const projectData = [
     link: null,
     description:
       "An educational app designed to help children learn new vocabulary in various languages.\nThe in-built camera app uses machine learning to reocgnise and translate household objects, and vocabulary quizzes help kids to memorise words.\nBuilt as a final project at Northcoders bootcamp with a team of four other people, in React Native and Python.",
-    url: null      
+    url: '../../public/images/uFluent.jpg'    
   },
 ];
