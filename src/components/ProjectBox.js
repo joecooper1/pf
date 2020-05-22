003a47373c375acc6458e7193378914ed50a41ee
Joe Cooper <joecooper2991@gmail.com>
2020-05-22 15:21:08 +0100

    map through projectdata

--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -3,11 +3,14 @@ import React from "react";
 import { Box } from "../styles/Projects";
 import Project from "./Project";
 
+import { projectData } from "../data/projectData";
+
 export default function ProjectBox(props) {
-  return <Box dimensions={props}>
-      <Project dimensions={props}/>
-      <Project dimensions={props}/>
-      <Project dimensions={props}/>
-      <Project dimensions={props}/>
-  </Box>;
+  return (
+    <Box dimensions={props}>
+      {projectData.map(data => {
+          return <Project dimensions={props} data={data} />
+      })}
+    </Box>
+  );
 }
