a85b2a619f6b75d788a9f05e378fe8b528407328
Joe Cooper <joecooper2991@gmail.com>
2020-05-23 10:52:14 +0100

    add ncnews to project data

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,5 +1,6 @@
-import onlyReggae from '../images/onlyReggae.jpg';
-import uFluent from '../images/uFluent.jpg';
+import onlyReggae from "../images/onlyReggae.jpg";
+import uFluent from "../images/uFluent.jpg";
+import ncNews from "../images/ncnews.png";
 
 export const projectData = [
   {
@@ -8,7 +9,7 @@ export const projectData = [
     link: null,
     description:
       "A small music app I made in four days for a tech test.\nIt uses the Napster API to display the top Reggae tracks, charts and playlists, and gives users the ability to search the database for their favourite albums, artists and songs. Built using React Native.",
-    url: onlyReggae   
+    url: onlyReggae,
   },
   {
     name: "uFluent",
@@ -16,6 +17,14 @@ export const projectData = [
     link: null,
     description:
       "An educational app designed to help children learn new vocabulary in various languages.\nThe in-built camera app uses machine learning to reocgnise and translate household objects, and vocabulary quizzes help kids to memorise words.\nBuilt as a final project at Northcoders bootcamp with a team of four other people, in React Native and Python.",
-    url: uFluent    
+    url: uFluent,
+  },
+  {
+    name: "NC News (frontend)",
+    github: "https://github.com/joecooper1/nc-news-frontend",
+    link: "https://nc-news-joecooper1.netlify.app/",
+    description:
+      "The frontend of my Reddit-like news app, built in React, using Axios, Reach Router and styled-components. It displays the most recent articles, the top users, 'trending' articles based on an algorithm I designed, and a search feature. The background is dynamic and made using CSS.",
+    url: ncNews,
   },
 ];
