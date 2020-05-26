db68233e40b3862d115e753a5ab4d772507c2cf2
Joe Cooper <joecooper2991@gmail.com>
2020-05-26 14:00:27 +0100

    add view around title

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,24 +1,26 @@
 import React from "react";
 
-import { Title, Text, View, SmallText, Link } from "../styles/Bio";
+import { Title, Text, View, SmallText, Link, SmallView } from "../styles/Bio";
 
 export default function Bio(props) {
   return (
     <View dimensions={props}>
-      <Title dimensions={props}>Joe Cooper</Title>
-      <Text dimensions={props}>
-        Hi. I'm Joe.<br></br> I'm a full-stack software developer.
-      </Text>
-      <SmallText dimensions={props}>
-        Email me at joecooper2991@gmail.com
-      </SmallText>
-      <SmallText dimensions={props}>
-        or here I am on{" "}
-        <Link href="https://www.github.com/joecooper1">github</Link> and{" "}
-        <Link href="https://www.linkedin.com/in/joe-cooper-202b2689/">
-          linkedin
-        </Link>
-      </SmallText>
+      <SmallView dimensions={props}>
+        {/* <Title dimensions={props}>Joe Cooper</Title> */}
+        <Text dimensions={props}>
+          Hi. I'm Joe.<br></br> I'm a full-stack software developer.
+        </Text>
+        <SmallText dimensions={props}>
+          Email me at joecooper2991@gmail.com
+        </SmallText>
+        <SmallText dimensions={props}>
+          or here I am on{" "}
+          <Link href="https://www.github.com/joecooper1">github</Link> and{" "}
+          <Link href="https://www.linkedin.com/in/joe-cooper-202b2689/">
+            linkedin
+          </Link>
+        </SmallText>
+      </SmallView>
     </View>
   );
 }
--- src/styles/Bio.js
+++ src/styles/Bio.js
@@ -5,7 +5,7 @@ export const Title = styled.h1`
     return props.dimensions.width > 1024 ? '2.5em' : '1.5em'
   }};
   text-align: center;
-  color: palevioletred;
+  // color: palevioletred;
   margin: 0;
 `;
 
@@ -14,7 +14,7 @@ export const Text = styled.p`
     return props.dimensions.width > 1024 ? '1em' : '0.7em'
   }};
   text-align: center;
-  color: palevioletred;
+  // color: palevioletred;
 `;
 
 export const SmallText = styled.p`
@@ -22,18 +22,24 @@ export const SmallText = styled.p`
     return props.dimensions.width > 1024 ? '0.7em' : '0.5em'
   }};
   text-align: center;
-  color: palevioletred;
+  // color: palevioletred;
   margin: 0;
 `;
 
-export const View = styled.header`
+export const View = styled.div`
   display: flex;
   flex-direction: column;
   height: ${props => `${props.dimensions.height}px`};
   justify-content: center;
 `;
 
+export const SmallView = styled.div`
+  background-color: rgba(64, 66, 108, 0.4);
+  padding: 40px;
+  border-radius: 20px;
+`;
+
 export const Link = styled.a`
-  color: palevioletred;
+  color: black;
   text-decoration: underline;
 `;
