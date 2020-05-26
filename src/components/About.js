834c45c9d0aeaf592eea33c449424d85b989fbde
Joe Cooper <joecooper2991@gmail.com>
2020-05-26 16:06:06 +0100

    add tech skills section

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,99 @@
 import React from "react";
 
-import { View, Text } from "../styles/About";
+import {
+  View,
+  Text,
+  Skills,
+  Section,
+  SmallText,
+  LogoBar,
+  Logo,
+  LogoText,
+} from "../styles/About";
+
+import {
+  FaJsSquare,
+  FaReact,
+  FaCss3Alt,
+  FaHtml5,
+  FaJava,
+} from "react-icons/fa";
 
 export default function About(props) {
   return (
-    <View>
-      <Text>I've been using code - terrible, convoluted, wildly inefficient code - in my artistic projects for years, to create interactive performances and small games for commissions. In 2019 I decided to make the leap into becoming a software developer full time, and took a three-month training course at Northcoders, in Manchester. I came out with a new understanding of what programming was, the ability to do with five lines of code what I used to be able to do with five hundred, and new ambitions as to what I could achieve with my new skills. Now I'm looking for opportunities to put those skills and my creativity to work in the world of software.</Text>
+    <View dimensions={props}>
+      <Text>
+        I've been using code - terrible, convoluted, wildly inefficient code -
+        in my artistic projects for years, to create interactive performances
+        and small games for commissions. In 2019 I decided to make the leap into
+        becoming a software developer full time, and took a three-month training
+        course at Northcoders, in Manchester. I came out with a new
+        understanding of what programming was, the ability to do with five lines
+        of code what I used to be able to do with five hundred, and new
+        ambitions as to what I could achieve with my new skills. Now I'm looking
+        for opportunities to put those skills and my creativity to work in the
+        world of software.
+      </Text>
+      <Skills>
+        <Section>
+          <SmallText>What I'm best at</SmallText>
+          <LogoBar>
+            <Logo>
+              <FaJsSquare size={60} />
+              <LogoText>JavaScript</LogoText>
+              <LogoText>NodeJS</LogoText>
+            </Logo>
+            <Logo>
+              <FaReact size={60} />
+              <LogoText>React</LogoText>
+              <LogoText>+ Native</LogoText>
+            </Logo>
+            <Logo>
+              <FaHtml5 size={60} />
+              <LogoText>HTML5</LogoText>
+            </Logo>
+            <Logo>
+              <FaCss3Alt size={60} />
+              <LogoText>CSS3</LogoText>
+            </Logo>
+          </LogoBar>
+        </Section>
+        <Section>
+          <SmallText>What I'm good at</SmallText>
+          <LogoBar>
+            <Logo>
+              <FaJava size={60} />
+              <LogoText>Java</LogoText>
+            </Logo>
+            <Logo>
+              <LogoText>PostgreSQL</LogoText>
+              <LogoText>Axios</LogoText>
+              <LogoText>Express</LogoText>
+              <LogoText>Knex</LogoText>
+            </Logo>
+            <Logo>
+              <LogoText>Mocha</LogoText>
+              <LogoText>Chai</LogoText>
+              <LogoText>Supertest</LogoText>
+              <LogoText>Jest</LogoText>
+            </Logo>
+          </LogoBar>
+        </Section>
+        <Section>
+          <SmallText>What I'm learning</SmallText>
+          <LogoBar>
+            <Logo>
+              <LogoText>Python</LogoText>
+            </Logo>
+            <Logo>
+              <LogoText>Android Studio</LogoText>
+            </Logo>
+            <Logo>
+              <LogoText>Ruby on Rails</LogoText>
+            </Logo>
+          </LogoBar>
+        </Section>
+      </Skills>
     </View>
   );
 }
--- src/styles/About.js
+++ src/styles/About.js
@@ -4,6 +4,11 @@ export const View = styled.div`
   width: 100%;
   padding: 30px;
   background-color: black;
+  display: flex;
+  flex-direction: ${(props) => {
+    return props.dimensions.width > 1024 ? "row" : "column";
+  }};
+  justify-content: flex-start;
 `;
 
 export const Text = styled.p`
@@ -12,4 +17,44 @@ export const Text = styled.p`
   margin-left: 5%;
   width: 50%;
   line-height: 2;
+`;
+
+export const Skills = styled.div`
+  margin: 0;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 30%;
+  margin-left: 10%;
+`;
+
+export const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const SmallText = styled.text`
+  font-size: 20px;
+`;
+
+export const LogoBar = styled.ul`
+  display: flex;
+  flex-direction: row;
+  padding-left: 0;
+`;
+
+export const Logo = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: 20px;
+  margin-right: 20px;
+`;
+
+export const LogoText = styled.p`
+  font-size: 15px;
+  margin: 0;
 `;
