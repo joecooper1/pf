b14133c2dd0b050611eebdd1531557bfe36648cc
Joe Cooper <joecooper2991@gmail.com>
2020-05-27 11:07:54 +0100

    Updates

---
+++ precache-manifest.53fc9048d32979b75b5d45c87c6e7261.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71de7dc71f43dedf0233dbaf247f1914",
+    "url": "/pf/index.html"
+  },
+  {
+    "revision": "54cd938cb5e17640157f",
+    "url": "/pf/static/css/2.043b003d.chunk.css"
+  },
+  {
+    "revision": "5a93fc70ebbae704c0de",
+    "url": "/pf/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "54cd938cb5e17640157f",
+    "url": "/pf/static/js/2.9cd5908f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pf/static/js/2.9cd5908f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a93fc70ebbae704c0de",
+    "url": "/pf/static/js/main.03ae4408.chunk.js"
+  },
+  {
+    "revision": "1fb168d2902f947f4ff3",
+    "url": "/pf/static/js/runtime-main.d23640c1.js"
+  },
+  {
+    "revision": "a14e3f31fb3fe1631e20ef0a85087103",
+    "url": "/pf/static/media/angles.a14e3f31.png"
+  },
+  {
+    "revision": "f19fc8e7c16dee2c9accf4439508732a",
+    "url": "/pf/static/media/beat.f19fc8e7.png"
+  },
+  {
+    "revision": "772e97a0cf7afe5e472d997447e8ebf1",
+    "url": "/pf/static/media/box.772e97a0.png"
+  },
+  {
+    "revision": "0c5efceecdf44180574de6faf06e764b",
+    "url": "/pf/static/media/car.0c5efcee.png"
+  },
+  {
+    "revision": "e646d08602035ad7fd46154731a7032f",
+    "url": "/pf/static/media/dead.e646d086.png"
+  },
+  {
+    "revision": "79f6a87e527d56627cceb5c82dd1e58c",
+    "url": "/pf/static/media/ghost.79f6a87e.png"
+  },
+  {
+    "revision": "ba779395eed6fd0538b54c36c3738e4c",
+    "url": "/pf/static/media/hand.ba779395.png"
+  },
+  {
+    "revision": "535c3b17352d6ac5bc50e6d2a1cdec83",
+    "url": "/pf/static/media/meditation.535c3b17.png"
+  },
+  {
+    "revision": "9bcf72451f671e90a151b858bd5c797d",
+    "url": "/pf/static/media/menu.9bcf7245.png"
+  },
+  {
+    "revision": "1ff45989e26bc6a1f021a2180982b834",
+    "url": "/pf/static/media/ncbackend.1ff45989.png"
+  },
+  {
+    "revision": "07bcfe28e3e171e8cd803d30b918cfb6",
+    "url": "/pf/static/media/ncnews.07bcfe28.png"
+  },
+  {
+    "revision": "efdb9fe5173dd240dee8aad016e678d0",
+    "url": "/pf/static/media/nightmare.efdb9fe5.png"
+  },
+  {
+    "revision": "9185b13b012e31722a99f388ceff36e4",
+    "url": "/pf/static/media/onlyReggae.9185b13b.jpg"
+  },
+  {
+    "revision": "3a34f702f10fe9c104595f72a3cf75f2",
+    "url": "/pf/static/media/rain.3a34f702.png"
+  },
+  {
+    "revision": "3a6ae31bdccc2b0aecb213eca5d466e5",
+    "url": "/pf/static/media/smiles.3a6ae31b.png"
+  },
+  {
+    "revision": "01dc2564f59302ebca82b637004f30f8",
+    "url": "/pf/static/media/space.01dc2564.png"
+  },
+  {
+    "revision": "8bdf874fbe765ba12db17c728e08db6c",
+    "url": "/pf/static/media/uFluent.8bdf874f.jpg"
+  }
+]);
