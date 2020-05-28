0cc116b4a507d5db087e0d4d0fd9fcb847dd2b65
Joe Cooper <joecooper2991@gmail.com>
2020-05-28 11:26:35 +0100

    rename component

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,29 +2,11 @@ import React, { useState, useEffect, useRef } from "react";
 
 import { MainCanvas } from "../styles/Canvas";
 
-import DynamicRainbowGridComponent from "../canvas-components/DynamicRaibowGridComponent";
+import DynamicRainbowGridComponent from "../canvas-components/DynamicRainbowGridComponent";
 
 export default function Canvas(props) {
-  // const canvasRef = useRef(null);
   const width = props.width > 1024 ? props.width * 0.9 : props.width;
   const height = props.width > 1024 ? props.height * 0.9 : props.height;
 
-  // useEffect(() => {
-  //   const canvas = canvasRef.current;
-  //   if (canvas) {
-  //     const ctx = canvas.getContext("2d");
-  //     dynamicRainbowGrid(ctx, width, height);
-  //   }
-  // });
-
-  return (
-    // <MainCanvas
-    //   id="main"
-    //   width={width}
-    //   height={height}
-    //   ref={canvasRef}
-    //   dimensions={props}
-    // ></MainCanvas>
-    <DynamicRainbowGridComponent height={height} width={width} />
-  );
+  return <DynamicRainbowGridComponent height={height} width={width} />;
 }
