d673d8314beb0b0a4dfa59ee2757bc43c2aeb302
Joe Cooper <joecooper2991@gmail.com>
2020-05-29 14:44:16 +0100

    make background image on a phone

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -3,10 +3,27 @@ import React from "react";
 import DynamicRainbowGridComponent from "../canvas-components/DynamicRainbowGridComponent";
 import TestCanvas from "../canvas-components/TestCanvas";
 
+import fancyBg1 from "../images/fancyBg1.png";
+import fancyBg2 from "../images/fancyBg2.png";
+import fancyBg3 from "../images/fancyBg3.png";
+
 export default function Canvas(props) {
   const width = props.width > 1024 ? props.width * 0.9 : props.width;
   const height = props.width > 1024 ? props.height * 0.9 : props.height;
 
-  return <DynamicRainbowGridComponent height={height} width={width} />;
-  // return <TestCanvas height={height} width={width}></TestCanvas>;
+  if (props.width > 1024) {
+    return <DynamicRainbowGridComponent height={height} width={width} />;
+    // return <TestCanvas height={height} width={width}></TestCanvas>;
+  } else {
+    //Place a background image
+    const pics = [fancyBg1, fancyBg2, fancyBg3];
+    const pic = pics[Math.floor(Math.random() * pics.length)];
+    return (
+      <img
+        src={pic}
+        alt="stained-glass-background"
+        style={{ width: props.width, zIndex: -1, position: 'absolute' }}
+      ></img>
+    );
+  }
 }
