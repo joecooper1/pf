f21d75d2bd6bff221a85017d8c6fbfd1cb1959a1
Joe Cooper <joecooper2991@gmail.com>
2020-05-29 14:05:25 +0100

    change margins

--- src/styles/Canvas.js
+++ src/styles/Canvas.js
@@ -4,7 +4,7 @@ export const MainCanvas = styled.canvas`
   background-color: rgba(0, 0, 0, 0.1);
   position: absolute;
   margin: ${(props) => {
-    return props.dimensions.width > 1024
+    return props.dimensions.width > 1024 * 0.9
       ? `${props.dimensions.height * 0.05}px ${props.dimensions.width * 0.05}px`
       : ``;
   }};
