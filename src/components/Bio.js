4f072cbf051fda1e5585f7988f3f02b30ff1630c
Joe Cooper <joecooper2991@gmail.com>
2020-06-02 13:52:26 +0100

    made second smallView for background info

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   Text,
@@ -8,12 +8,19 @@ import {
   SmallView,
   ButtonsBar,
   Button,
-  ButtonTwo
+  ButtonTwo,
+  OptionsBar,
+  TinyText,
+  SelectBar,
 } from "../styles/Bio";
 
 export default function Bio(props) {
-  return (
-    <View dimensions={props}>
+  const [info, setInfo] = useState("main");
+
+  let infoToShow = <h1>what</h1>;
+
+  if (info === "main") {
+    infoToShow = (
       <SmallView dimensions={props}>
         <Text dimensions={props}>
           Hi. I'm Joe.<br></br> I'm a full-stack software developer.
@@ -46,8 +53,45 @@ export default function Bio(props) {
             projects
           </Button>
         </ButtonsBar>
-        <ButtonTwo>what is this background?</ButtonTwo>
+        <ButtonTwo
+          onClick={() => {
+            setInfo("background");
+          }}
+        >
+          what is this background?
+        </ButtonTwo>
       </SmallView>
-    </View>
-  );
+    );
+  } else {
+    //Background info
+    infoToShow = (
+      <SmallView dimensions={props}>
+        <TinyText dimensions={props}>
+          The background is drawn using stained-glass-generator,<br></br> an NPM
+          package that I made.
+        </TinyText>
+        <br></br>
+        <TinyText dimensions={props}>
+          View it{" "}
+          <Link href="https://www.npmjs.com/package/@joecooper1/stained-glass-generator">
+            here
+          </Link>
+          on npm<br></br>or{" "}
+          <Link href="https://github.com/joecooper1/stained-glass-generator">
+            here
+          </Link>{" "}
+          on github.
+        </TinyText>
+        <ButtonTwo
+          onClick={() => {
+            setInfo("main");
+          }}
+        >
+          go back
+        </ButtonTwo>
+      </SmallView>
+    );
+  }
+
+  return <View dimensions={props}>{infoToShow}</View>;
 }
