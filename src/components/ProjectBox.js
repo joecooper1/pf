f7d261f1c17a2c635d68813ea7a03b60d4e41912
Joe Cooper <joecooper2991@gmail.com>
2020-06-03 13:26:53 +0100

    remove portfolio title

--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -14,7 +14,7 @@ import { projectData } from "../data/projectData";
 export default function ProjectBox(props) {
   return (
     <Portfolio ref={props.projectsRef}>
-      <PortfolioTitle dimensions={props}>Portfolio</PortfolioTitle>
+      {/* <PortfolioTitle dimensions={props}>Portfolio</PortfolioTitle> */}
       <Box dimensions={props}>
         {projectData.map((data) => {
           return <Project dimensions={props} data={data} key={data.name} />;
